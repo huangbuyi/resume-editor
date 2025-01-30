50398d4ca6e0fa7333ee020513296c2dded0d9bc
huang4bin <huang4bin@gmail.com>
2025-01-30 17:48:35 +0800

    feat(template): 新增侧边栏模板

--- src/templates/templates.contribution.ts
+++ src/templates/templates.contribution.ts
@@ -1,6 +1,7 @@
 import { registerTemplate } from '../market/register';
 import { ClassicVertical } from './ClassicVertical';
 import { TowColumns } from './TowColumns';
+import { Sider } from './Sider';
 
 registerTemplate({
   name: 'ClassicVertical',
@@ -13,3 +14,9 @@ registerTemplate({
   title: '双栏布局',
   template: TowColumns,
 });
+
+registerTemplate({
+  name: 'Sider',
+  title: '侧边栏',
+  template: Sider
+})
