21ba0c5ffb60450957ae17422594c164dcf9c9aa
huang4bin <huang4bin@gmail.com>
2025-01-30 20:17:47 +0800

    feat(template): 新增混合双栏模板

--- src/templates/templates.contribution.ts
+++ src/templates/templates.contribution.ts
@@ -2,6 +2,7 @@ import { registerTemplate } from '../market/register';
 import { ClassicVertical } from './ClassicVertical';
 import { TowColumns } from './TowColumns';
 import { Sider } from './Sider';
+import { MixTowColumns } from './MixTowColumns';
 
 registerTemplate({
   name: 'ClassicVertical',
@@ -19,4 +20,10 @@ registerTemplate({
   name: 'Sider',
   title: '侧边栏',
   template: Sider
+})
+
+registerTemplate({
+  name: 'MixTowColumns',
+  title: '混合双栏布局',
+  template: MixTowColumns
 })
