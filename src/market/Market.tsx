b2b281c804c9a8c95337abf46f9f67424784ca56
Huang Shibin <huang4bin@gmail.com>
2025-02-05 13:46:11 +0800

    feat(market): 优化模板市场筛选功能
    - 新增色彩和栏数筛选选项
    - 添加更多筛选和收起筛选功能
    - 优化页面布局和样式
    - 增加无结果时的空状态提示

--- src/market/Market.tsx
+++ src/market/Market.tsx
@@ -1,22 +1,36 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { FilterOptions, TemplateOptions, templateRegistry } from './register';
-import { Flex, Form, Pagination, Radio, Tooltip } from 'antd';
+import { Button, Empty, Flex, Form, Pagination, Radio, Tooltip } from 'antd';
 import { useResumeStore } from '../resume/store';
 import styles from './market.module.css';
 import { TemplatePreview } from '../preview/TemplatePreview';
 import { useTemplateStore } from '../resume/template';
 import { useNavigate } from 'react-router-dom';
 import { CheckboxGroupProps } from 'antd/es/checkbox';
-import { QuestionCircleTwoTone } from '@ant-design/icons';
+import { DownOutlined, QuestionCircleTwoTone, UpOutlined } from '@ant-design/icons';
 
 const marginOptions: CheckboxGroupProps<string>['options'] = [
   { label: '全部', value: '' },
   { label: '安全边距', value: 'safe' },
   { label: '小、无边距', value: 'noSafe' }
 ];
-
 const safeMarginTip = '安全边距指的是页面边缘与内容之间的距离，它保证了所有关键信息都不会因为打印或装订过程中的误差而被裁剪掉或难以阅读';
-const pageSize = 10;
+
+const colorOptions: CheckboxGroupProps<string>['options'] = [
+  { label: '全部', value: '' },
+  { label: '黑白', value: 'grayscale' },
+  { label: '彩色', value: 'color' },
+];
+const colorTip = '投递彩色简历可能导致打印时出现颜色失真、对比度不足以及增加打印成本的问题，尤其在黑白打印情况下可能影响简历的可读性和专业呈现。';
+
+const columnOptions: CheckboxGroupProps<string>['options'] = [
+  { label: '全部', value: '' },
+  { label: '单栏', value: 'single' },
+  { label: '双栏', value: 'double' },
+  { label: '三栏以上', value: 'more' },
+];
+
+const pageSize = 12;
 
 export function Market() {
   const resume = useResumeStore();
@@ -25,9 +39,20 @@ export function Market() {
   const navigate = useNavigate();
   const [filter, setFilter] = useState<FilterOptions>({
     margin: '',
-    color: ''
+    color: '',
+    column: '',
   });
+  const [showMoreFilter, setShowMoreFilter] = useState(false);
   const [templateData, setTemplates] = useState<{ total: number; templates: TemplateOptions[]}>({ total: 0, templates: []  });
+  const filterCount = useMemo(() => {
+    let count = 0;
+    for (const key in filter) {
+      if (filter[key as keyof FilterOptions] !== '') {
+        count++;
+      }
+    }
+    return count;
+  }, [filter]);
 
   const updateTemplates = () => {
     setTemplates(templateRegistry.getTemplates(page, pageSize, filter));
@@ -47,31 +72,56 @@ export function Market() {
 
   return (
     <div className={styles.market}>
-      <Form size="small" labelCol={{ span: 4 }}>
-        <Form.Item label={<span>页面边距 <Tooltip placement="bottomRight" title={safeMarginTip}><QuestionCircleTwoTone /></Tooltip></span>}>
-          <Radio.Group
-            value={filter.margin}
-            options={marginOptions}
-            optionType="button"
-            buttonStyle="solid"
-            onChange={e => handleFilterChange({ margin: e.target.value })}>
-          </Radio.Group>
-        </Form.Item>
-      </Form>
-      <Pagination current={page} pageSize={pageSize} total={templateData.total} onChange={setPage} />
-      <Flex gap={32} wrap="wrap" justify="center">
+      <Flex justify="center">
+        <Form size="small" labelCol={{ span: 5 }} style={{ width: '520px', height: showMoreFilter ? undefined : '36px', overflow: 'hidden' }}>
+          <Form.Item label={<span>页面边距 <Tooltip placement="bottomRight" title={safeMarginTip}><QuestionCircleTwoTone /></Tooltip></span>}>
+            <Radio.Group
+              value={filter.margin}
+              options={marginOptions}
+              optionType="button"
+              buttonStyle="solid"
+              onChange={e => handleFilterChange({ margin: e.target.value })}>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label={<span>色彩 <Tooltip placement="bottomRight" title={colorTip}><QuestionCircleTwoTone /></Tooltip></span>}>
+            <Radio.Group
+              value={filter.color}
+              options={colorOptions}
+              optionType="button"
+              buttonStyle="solid"
+              onChange={e => handleFilterChange({ color: e.target.value })}>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label={<span>栏数</span>}>
+            <Radio.Group
+              value={filter.column}
+              options={columnOptions}
+              optionType="button"
+              buttonStyle="solid"
+              onChange={e => handleFilterChange({ column: e.target.value })}>
+            </Radio.Group>
+          </Form.Item>
+        </Form>
         {
-          templateData.templates.map((template) => (
+          showMoreFilter ?
+            <Button type="text" size="small" icon={<UpOutlined />} onClick={() => setShowMoreFilter(false)}>收起筛选 {filterCount > 0 ? `(${filterCount})` : null}</Button> :
+            <Button type="text" size="small" icon={<DownOutlined />} onClick={() => setShowMoreFilter(true)}>更多筛选 {filterCount > 0 ? `(${filterCount})` : null}</Button>
+        }
+      </Flex>
+      <Pagination current={page} align="center" pageSize={pageSize} total={templateData.total} onChange={setPage} />
+      <Flex gap={32} wrap="wrap" justify="center" style={{ margin: '16px 0' }}>
+        {
+          templateData.templates.length > 0 ? templateData.templates.map((template) => (
             <div key={template.name} className={styles.template}>
               <div className={styles.preview} onClick={() => changeTemplate(template.name)}>
                 <TemplatePreview template={<template.template resume={resume} />} home={template.home} margin={template.margin} />
               </div>
               <div className={styles.title}>{template.title}</div>
             </div>
-          ))
+          )) : <Empty />
         }
       </Flex>
-      <Pagination current={page} pageSize={pageSize} total={templateData.total} onChange={setPage} />
+      <Pagination current={page} align="center" pageSize={pageSize} total={templateData.total} onChange={setPage} />
     </div>
   )
 }
