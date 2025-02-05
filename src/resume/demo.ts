992ce1073246c55fe5b70e85d2572ec440e21028
Huang Shibin <huang4bin@gmail.com>
2025-02-05 16:12:27 +0800

    refactor(resume): 将王小美设置默认示例

--- src/resume/demo.ts
+++ src/resume/demo.ts
@@ -7,16 +7,16 @@ export interface DemoOptions {
 
 export const demos = [
   {
-    name: 'zhangwei',
-    local: '张伟',
-    title: '产品经理',
-    url: 'demos/zhangwei.json'
-  },
-  {
     name: 'wangxiaomei',
     local: '王小美',
     title: 'UI/UX 设计师',
     url: 'demos/wangxiaomei.json'
+  },
+  {
+    name: 'zhangwei',
+    local: '张伟',
+    title: '产品经理',
+    url: 'demos/zhangwei.json'
   },
 ]
 
